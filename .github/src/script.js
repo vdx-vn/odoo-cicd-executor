ff3474b99cce6e8f5fd6d41fc31906e46e4e8488
xmars4 <xtruongptit@gmail.com>
2024-08-30 17:11:41 +0700

    up

---
+++ .github/src/script.js
@@ -0,0 +1,21 @@
+export default function createRunCheck() {
+    const inputs = ${{ toJSON(inputs) }};
+    const repo_data = inputs.repo_name.split('/');
+    const commit_sha = inputs.commit_sha;
+    const owner = repo_data[0];
+    const repo = repo_data[1];
+
+    console.log(owner);
+    console.log(repo);
+
+    const response = await github.rest.checks.create({
+        owner: owner,
+        repo: repo,
+        head_sha: commit_sha,
+        name: "Oh yeah luon",
+        status: "in_progress"
+    });
+
+    return response.data.id;
+}
+    
