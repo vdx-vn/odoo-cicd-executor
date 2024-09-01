693fa73cc0715e7bb7fbdd8a6d7d5e3a8bb4374c
xmars4 <xtruongptit@gmail.com>
2024-09-01 12:53:32 +0700

    up

--- .github/src/script.js
+++ .github/src/script.js
@@ -15,3 +15,17 @@ export async function createRunCheck(github, inputs, name, status = "in_progress
 
     return response.data.id;
 }
+
+export async function updateRunCheck(github, inputs, check_run_id, status = "completed") {
+    // https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#create-a-check-run
+    const repo_data = inputs.repo_name.split("/");
+    const owner = repo_data[0];
+    const repo = repo_data[1];
+
+    const response = await github.rest.checks.update({
+        owner,
+        repo,
+        check_run_id,
+        status,
+    });
+}
