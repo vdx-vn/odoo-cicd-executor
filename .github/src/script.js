ee9e91318c5b0a9c407c49da01a9c475f824cb41
xmars4 <xtruongptit@gmail.com>
2024-09-05 13:35:49 +0700

    up

--- .github/src/script.js
+++ .github/src/script.js
@@ -4,6 +4,7 @@ async function createRunCheck(github, inputs, name, status = "in_progress") {
     const commit_sha = inputs.commit_sha;
     const owner = repo_data[0];
     const repo = repo_data[1];
+    const check_url = `${github.server_url}/${github.repository}/actions/runs/${github.run_id}`;
 
     const response = await github.rest.checks.create({
         owner,
@@ -16,13 +17,14 @@ async function createRunCheck(github, inputs, name, status = "in_progress") {
             summary: "",
             text: "",
         },
+        details_url: check_url,
     });
 
     return response.data.id;
 }
 
 async function updateRunCheck(github, inputs, check_run_id, status = "completed", conclusion = "success") {
-    // https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#create-a-check-run
+    // https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#update-a-check-run
     const repo_data = inputs.repo_name.split("/");
     const owner = repo_data[0];
     const repo = repo_data[1];
