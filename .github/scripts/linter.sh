3087429cf6c619d776599501faac0c8feab9a245
xmars4 <xtruongptit@gmail.com>
2025-01-14 11:49:36 +0700

    separate linter to bash script

---
+++ .github/scripts/linter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source "${CICD_UTILS_SCRIPTS_PATH}"
+
+function lint() {
+    docker run \
+        -e LOG_LEVEL=WARN \
+        -e FILTER_REGEX_EXCLUDE=__manifest__\.py \
+        -e RUN_LOCAL=true \
+        -e USE_FIND_ALGORITHM=true \
+        -v $REPO_PATH:/tmp/lint \
+        ghcr.io/super-linter/super-linter:latest
+
+    summary=$REPO_PATH/super-linter-output/super-linter-summary.md
+
+    send_file_telegram_default $summary "Linter result"
+}
+
+lint "$@"
