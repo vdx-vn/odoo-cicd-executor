37163455081e71c5b2dd2aeb8944d37fd5b02df7
xmars4 <xtruongptit@gmail.com>
2025-01-14 12:05:22 +0700

    up

--- .github/scripts/linter.sh
+++ .github/scripts/linter.sh
@@ -4,15 +4,17 @@ source "${CICD_UTILS_SCRIPTS_PATH}"
 
 function lint() {
     docker run \
-        -e LOG_LEVEL=WARN \
+        -e LOG_LEVEL=INFO \
         -e FILTER_REGEX_EXCLUDE=__manifest__\.py \
         -e RUN_LOCAL=true \
         -e USE_FIND_ALGORITHM=true \
         -e SAVE_SUPER_LINTER_SUMMARY=true \
+        -e SAVE_SUPER_LINTER_OUTPUT=true \
         -v $REPO_PATH:/tmp/lint \
         ghcr.io/super-linter/super-linter:latest
 
     summary=$REPO_PATH/super-linter-output/super-linter-summary.md
+    output=$REPO_PATH/super-linter-output/super-linter
 
     echo "==============================="
     echo "==============================="
@@ -26,7 +28,7 @@ function lint() {
     echo "==============================="
     echo "==============================="
 
-    send_file_telegram_default "$summary" "Linter result"
+    send_file_telegram_default "$output" "Linter result"
 }
 
 lint "$@"
