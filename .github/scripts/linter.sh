9bf9cd71143b5bf0269adf4c287263ab228385e5
xmars4 <xtruongptit@gmail.com>
2025-01-14 12:49:38 +0700

    up

--- .github/scripts/linter.sh
+++ .github/scripts/linter.sh
@@ -22,14 +22,13 @@ function lint() {
     echo "==============================="
     echo "==============================="
     cat $summary
-    ls -lah $REPO_PATH/super-linter-output
     echo "==============================="
     echo "==============================="
     echo "==============================="
     echo "==============================="
     echo "==============================="
 
-    send_file_telegram_default "$summary" "Linter result"
+    send_file_telegram_default "$output" "Linter result"
 }
 
 lint "$@"
