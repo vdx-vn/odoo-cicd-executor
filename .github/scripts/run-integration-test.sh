a18edacbf43de28cb1260b7ce48ce80a550f7ca7
novmah62 <at180532@actvn.edu.vn>
2025-09-05 10:12:28 +0700

    feat(ci): add failure notification via Telegram

--- .github/scripts/run-integration-test.sh
+++ .github/scripts/run-integration-test.sh
@@ -180,7 +180,7 @@ EOF
 # EOF
 #     )
 
-    telegram_failed_message=$(create_telegram_failed_message "$type_message" "$PR_NUMBER" "$PR_URL" "$COMMIT_AUTHOR" "$sad_emojis")
+    telegram_failed_message=$(create_telegram_failed_message "Integration Test" "$PR_NUMBER" "$PR_URL" "$COMMIT_AUTHOR" "$sad_emojis")
 
     analyze_log_file "$failed_message" "$telegram_failed_message"
 }
