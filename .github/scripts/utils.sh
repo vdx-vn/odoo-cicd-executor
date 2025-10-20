a1795c8b56e3c3af1034103e54beb2a3af7dba2f
Mạnh <135102641+novmah62@users.noreply.github.com>
2025-10-20 17:28:06 +0700

    Fix/pipeline (#174)
    * fix(ci): comment out start_db_container function
    
    * fix(ci): comment out start_db_container function
    
    * fix(ci): comment out start_db_container function

--- .github/scripts/utils.sh
+++ .github/scripts/utils.sh
@@ -417,27 +417,26 @@ function analyze_log_file {
     show_separator "$success_message"
 }
 
-function start_db_container() {
-    docker run -d \
-        -p 5432:5432 \
-        --mount type=bind,source=$DOCKER_FOLDER/postgresql,target=/etc/postgresql \
-        -e POSTGRES_PASSWORD=odoo -e POSTGRES_USER=odoo -e POSTGRES_DB=postgres \
-        --name db \
-        $DB_IMAGE_TAG \
-        -c 'config_file=/etc/postgresql/postgresql.conf'
-}
+#function start_db_container() {
+#    docker run -d \
+#        -p 5432:5432 \
+#        --mount type=bind,source=$DOCKER_FOLDER/postgresql,target=/etc/postgresql \
+#        -e POSTGRES_PASSWORD=odoo -e POSTGRES_USER=odoo -e POSTGRES_DB=postgres \
+#        --name db \
+#        $DB_IMAGE_TAG \
+#        -c 'config_file=/etc/postgresql/postgresql.conf'
+#}
 
 function start_odoo_container() {
     docker run -d \
         --mount type=bind,source=$ODOO_ADDONS_PATH,target=/mnt/custom-addons \
         --mount type=bind,source=$DOCKER_FOLDER/etc,target=/etc/odoo \
         --mount type=bind,source=$DOCKER_FOLDER/logs,target=/var/log/odoo \
-        --link db:db \
         $ODOO_IMAGE_TAG
 }
 
 function start_containers() {
-    start_db_container
+#    start_db_container
     start_odoo_container
 }
 
