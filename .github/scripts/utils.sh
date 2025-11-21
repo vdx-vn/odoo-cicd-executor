0bad40f4aef3ffc93171f2e28da06fb9f79367e2
novmah62 <at180532@actvn.edu.vn>
2025-11-21 11:36:23 +0700

    fix(ci): update integration test runner to use ubuntu-latest

--- .github/scripts/utils.sh
+++ .github/scripts/utils.sh
@@ -387,8 +387,7 @@ function random_sad_emojis() {
 }
 
 function get_odoo_container_id {
-    cd $docker_compose_path
-    docker ps -q -a | xargs docker compose inspect --format '{{.Id}} {{.Config.Image}}' | awk -v img="${ODOO_IMAGE_TAG}" '$2 == img {print $1}'
+    docker ps -q -a | xargs docker inspect --format '{{.Id}} {{.Config.Image}}' | awk -v img="${ODOO_IMAGE_TAG}" '$2 == img {print $1}'
 }
 
 function docker_odoo_exec {
